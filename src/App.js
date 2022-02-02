88c1f8075060ac4c2c57ac70da42a9ec84dc4a56
tarrmmy <tarrmmy@gmail.com>
2022-02-02 13:40:48 +0100

    Recipe

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import './App.css';
   },[query]);
 
  const getRecipes = async () =>{
- const response = await fetch( `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+ const response = await fetch( `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
  const data = await response.json();
  setRecipes(data.hits); 
 }
@@ -31,8 +31,9 @@ import './App.css';
   return(
     <div className="App">
       <form onSubmit={getSearch} className="form">
-        <input className="bar" type="text" value={search} onChange={updateSearch}/>
-        <button className="button" type="submit">Search recipe</button>
+        <i className='fas fa-hamburger' style={{fontSize:'36px'}} />
+        <input placeholder="Search recipe..." className="bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="button" type="submit"><i class='fas fa-search' style={{fontSize:'20px'}}/>Search</button>
       </form>
       <div className="recipe">
        {recipes.map(recipe =>(
